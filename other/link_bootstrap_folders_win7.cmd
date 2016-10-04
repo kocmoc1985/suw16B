737b42e87d4dca80f18762ad9d87fbbf84054a1e
KOCMOC <KOCMOC@192.168.2.12>
2016-10-04 16:30:46 +0200

    V1.33 - HPWORK

---
+++ other/link_bootstrap_folders_win7.cmd
@@ -0,0 +1,16 @@
+cd..
+cd..
+cd..
+cd..
+cd..
+cd..
+cd xampp
+cd htdocs
+cd suw16b
+cd www
+cd workshops
+cd bootstrap
+mklink /d css ..\..\..\node_modules\bootstrap\dist\css
+mklink /d fonts ..\..\..\node_modules\bootstrap\dist\fonts
+mklink /d js ..\..\..\node_modules\bootstrap\dist\js
+pause
