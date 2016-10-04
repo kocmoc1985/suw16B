f170c648516f6a463ec851917c6009bcc87d1b40
ab <kocmoc1985@gmail.com>
2016-10-04 22:48:07 +0200

    V1.35 - DELL

---
+++ www/templates/menues/navbarLeftRight/menu.js
@@ -0,0 +1,90 @@
+$(document).ready(function () {
+    //
+    navBarFixedPositionFix("menu-container", "example-container");
+    //
+    addEventsToLinks();
+});
+
+function addEventsToLinks() {
+    var elemntArray = $(".a-mobile-onclick-expand").get();
+    for (i = 0; i < elemntArray.length; i++) {
+        $(elemntArray[i]).click(function(event) {
+            event.preventDefault();
+            fadeIn(this);
+        });
+    }
+    //
+    var elemntArray = $(".a-can-be-active").get();
+    for (i = 0; i < elemntArray.length; i++) {
+        $(elemntArray[i]).click(function(event) {
+            setActive(this);
+        });
+    }
+}
+
+/**
+ * This one is needed if you want to have "fixed" position for the NavBar
+ * @param {type} navBarContainerId - the id of the container
+ * @param {type} elemToSetMarginTopOn - id of the element which is direct under the navbar
+ * @returns {unresolved}
+ */
+function navBarFixedPositionFix(navBarContainerId, elemToSetMarginTopOn) {
+    //
+    var pos = $("#" + navBarContainerId).css("position");
+    //
+    console.log("css-position: " + pos);
+    //
+    if (pos === "fixed" === false) {
+        return;
+    }
+    //
+    var height = $("#" + navBarContainerId).outerHeight();
+    var elemMarginTop = document.getElementById(elemToSetMarginTopOn);
+    elemMarginTop.style.marginTop = height + "px";
+}
+
+
+function fadeIn(element) {
+    var parent = element.parentNode;
+    var elemntArray = parent.childNodes;
+    for (i = 0; i < elemntArray.length; i++) {
+        if (elemntArray[i].tagName === "UL") {
+            if ($(elemntArray[i]).is(':visible') === false) {
+                $(elemntArray[i]).css("display", "block");
+                $(elemntArray[i]).css({opacity: 0, visibility: "visible"}).animate({opacity: 1}, 500);
+            } else {
+                $(elemntArray[i]).fadeOut();
+            }
+        }
+    }
+}
+
+function setActive(element) {
+    //
+    resetAllActives();
+    element.className = "activeLink";
+    //
+    hideMenuIfMobile(element);
+}
+
+function hideMenuIfMobile(element) {
+    if ($(".li-mobile-menu").is(':visible')) {
+        $(element).parent().parent().fadeOut();
+    }
+}
+
+function resetAllActives() {
+    //
+    var elemntArray = $("." + "activeLink").get();
+    //
+    for (i = 0; i < elemntArray.length; i++) {
+        elemntArray[i].className = "none";
+    }
+}
+
+function imageNotFound(imgElement){
+    var parent = imgElement.parentNode;
+    parent.removeChild(imgElement);
+    $(parent).css("font-size","14pt");
+    $(parent).text(".....");
+}
