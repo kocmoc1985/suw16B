ec4d9890a9bca8dd5ba8f23846610b3a3a998c8e
KOCMOC <KOCMOC@192.168.2.12>
2016-10-21 11:52:05 +0200

    V1.56 - HPWORK

---
+++ www/templates/mixcont/js/main.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+    initialize();
+});
+
+function initialize() {
+    //
+    includeHtml("navbar/navbar.html", "#menu-container");
+    //
+    navBarFixedPositionFix("menu-container", "content");
+    //
+    addEventsToNavBarBtns();
+    //
+    addEventsToLinks();
+    //
+    includeHtml("content/splash.html", "#content");
+    //
+    includeHtml("content/customers.html", "#content","after");
+    //
+    includeHtml("footer/footer.html", "#footer-container-main");
+}
+
+function addEventsToNavBarBtns() {
+    addClickEventToNavBarBtn("#products-link", productsLinkClicked);
+}
+
+function productsLinkClicked() {
+    $("#customers-show").remove();
+    $("#content").empty();
+    includeHtmlAsync("content/products/products.html", "#content");
+}
+
+function includeHtml(url, selector, addType) {
+    //
+    var html = $.ajax({
+        url: url,
+        dataType: 'text',
+        async: false
+    }).responseText;
+    //
+    if (addType === "append") {
+        $(selector).append(html);
+    } else if (addType === "prepend") {
+        $(selector).prepend(html);
+    } else if (addType === "after") {
+        $(selector).after(html);
+    } else if (addType === "before") {
+        $(selector).before(html);
+    } else {
+        $(selector).append(html);
+    }
+}
+
+function includeHtmlAsync(url, selector, addType) {
+    $.ajax({
+        url: url,
+        dataType: 'text',
+        async: false
+    }).done(function(msg) {
+        if (addType === "append") {
+            $(selector).append(msg);
+        } else if (addType === "prepend") {
+            $(selector).prepend(msg);
+        } else if (addType === "after") {
+            $(selector).after(msg);
+        } else if (addType === "before") {
+            $(selector).before(msg);
+        } else {
+            $(selector).append(msg);
+        }
+    });
+}
