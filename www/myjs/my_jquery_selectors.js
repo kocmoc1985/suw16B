56f114bf8f2e528abc680e8c126465946434e4ab
KOCMOC <KOCMOC@192.168.2.12>
2016-11-14 15:48:02 +0100

    V1.67 - HPWORK

--- www/myjs/my_jquery_selectors.js
+++ www/myjs/my_jquery_selectors.js
@@ -1,3 +1,5 @@
+http://www.w3schools.com/jquery/jquery_ref_selectors.asp
+
 var value1 = $('input#username').val(); // textfield input value
 var value2 = $('input:checkbox:checked').val(); // get the value from a checked checkbox
 var value3 = $('input:radio[name=bar]:checked').val(); // get the value from a set of radio buttons
