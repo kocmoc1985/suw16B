232a9b1a5078a29aca6c872fe836b6b502856db9
ab <kocmoc1985@gmail.com>
2016-12-05 13:17:20 +0100

    v1.72 - DELL

---
+++ other/node.js/start node and firefox.cmd
@@ -0,0 +1,19 @@
+cd..
+cd..
+cd..
+cd..
+c:
+cd program files
+cd Mozilla Firefox
+firefox.exe http://localhost:3000/index.html
+cd..
+cd..
+cd..
+cd..
+cd..
+c:
+cd xampp
+cd htdocs
+cd suw16b_v47_gr_uppg
+node app.js
+pause
