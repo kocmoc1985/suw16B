fba5e51ac45090e95e7b4d24ae545386fcec1086
KOCMOC <KOCMOC@192.168.2.13>
2017-02-15 13:29:27 +0100

    v1.92 - HPWORK

---
+++ www/myjs/my_lib_iterators.js
@@ -0,0 +1,21 @@
+
+/**
+ * ITERATE ATTRIBUTES OF AN OBJECT
+ * @returns {undefined}
+ */
+function iterateA(obj){
+     $.each(obj, function (name, value) {
+         
+     });
+}
+
+/**
+ * ITERATE OBJECTS OF AN ARRAY
+ * @param {type} data
+ * @returns {undefined}
+ */
+function iterateB(array){
+     $(array).each(function (index, value) {
+         
+     });
+}
